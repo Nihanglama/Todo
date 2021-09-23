de3529c5eb885adeeca273c4686d3d64a679c675
Nihanglama <nihanglama@gmail.com>
2021-09-23 22:31:44 +0545

    change in Ui and somefunctions

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -109,3 +109,7 @@ LOGIN_URL='login'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL='/image/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
