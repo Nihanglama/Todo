24c9fc834d5e995322deb3667d35c9fe159e2c63
Nihanglama <nihanglama@gmail.com>
2021-09-24 08:36:45 +0545

    Updated Readme.md

--- todo/todos/urls.py
+++ todo/todos/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('register/',Register.as_view(),name='register'),
     path('user-login/',Login.as_view(),name='login'),
     path('user-logout/',LogoutView.as_view(next_page='login'),name='logout'),
-    path('list/',TaskList.as_view(),name='list'),
+    path('',TaskList.as_view(),name='list'),
     path('detail/<int:pk>/',DetailList.as_view(),name='detail'),
     path('create',Create.as_view(),name='create'),
     path('update/<int:pk>/',  Update.as_view(),name='update'),
